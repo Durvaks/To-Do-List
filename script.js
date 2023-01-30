0e3b7872902a35927662b131c039532fa370e43a
Renan_Brandao <104158547+Durvaks@users.noreply.github.com>
2023-01-30 07:47:39 -0300

    New update - 1.1
    update include:
    Correction for remove button (js) - where it was not uptade the cache.
    optons now is a object. (js)
    start a new layout. (css)

--- script.js
+++ script.js
@@ -6,35 +6,34 @@ const btn_clearLastItem = document.querySelector('.clear_last_item');
 const btn_clearFirstItem = document.querySelector('.clear_first_item');
 
 
-// clear all
-btn_clearCache.addEventListener("click", () => {
-  localStorage.clear();
-  taskList.innerHTML = "";
-  tasksCount = 0;
-});
-
-
 // Load any existing to-dos from cache
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-let tasksCount = 0;
+
 
 // Render the existing to-dos to the page
 tasks.forEach(task => {
-  //task agora retorna como um objeto
   taskList.appendChild(createLi(task));
 });
+
 
 // Add a new to-do when the form is submitted
 form.addEventListener('submit', event => {
   event.preventDefault();
 
+
+
   // Get the value of the input field
   const task = event.target.elements.task.value;
+
+
   if (task !== "") {
     const elementLi = createLi(task);
 
     // Add the task to the tasks array
-    tasks.push([elementLi.firstChild.innerText, elementLi.lastChild.innerText]); // <===### configurar para puxar array com itens ao inves de string
+    tasks.push({
+      "description": elementLi.firstChild.innerText,
+      "stats": elementLi.lastChild.firstChild.innerText
+    });
 
     // Render the new task to the page
     taskList.appendChild(elementLi);
@@ -47,52 +46,98 @@ form.addEventListener('submit', event => {
   }
 });
 
-// create a item List
+// EVENT BUTTON - clear all
+btn_clearCache.addEventListener("click", () => {
+  localStorage.clear();
+  taskList.innerHTML = "";
+});
+
+// ######  FUNCTION - create a item List  #######
 function createLi(task) {
 
-  const taskItem = document.createElement('li');
-  taskItem.classList.add(`li-${tasksCount}`);
-  tasksCount++;
+  const ListItem = document.createElement('li');
   const description = document.createElement('span');
-  const spanButton = document.createElement('span');
-  spanButton.classList.add("button_li");
+  const btnStats = document.createElement('span');
+  btnStats.classList.add("button_li");
+  const btnRemove = document.createElement("span");
+  btnRemove.classList.add("btnRemove");
+  btnRemove.innerText = "X";
+  const divButtons = document.createElement("div");
+  divButtons.classList.add("divButtons");
 
-  // with cache return
-  if (typeof (task) == "object") {
-    description.innerText = task[0];
-    spanButton.innerText = task[1];
+  const options = [
+    "novo item",
+    "pendente",
+    "feito"
+  ];
 
-    // change style with stats
-    switch (task[1]) {
-      case "Em tratativa":
-        taskItem.style.backgroundColor = "#ebe39e";
-        break;
-      case "Feito":
-        taskItem.style.backgroundColor = "#ccc";
-        break;
-      default:
-        break;
-    }
-  } else {
-    spanButton.innerText = "Em Tratativa";
-    description.innerText = task;
+  const statsColors = {
+    "novo item": "#ebe39e",
+    "novo item BTN": "white",
+
+    "pendente": "#ccc",
+    "pendente BTN": "yellow",
+
+    "feito": "#ccc",
+    "feito BTN": "green"
   }
 
-  // event button to stats
-  spanButton.addEventListener("click", (event) => {
+  // if from cache
+  if (typeof (task) == "object") {
+    description.innerText = task.description;
+    btnStats.innerText = task.stats;
+
+  } else {
+    description.innerText = task;
+    btnStats.innerText = options[0];
+  }
+  ListItem.classList.add(`li-${document.querySelectorAll('#task-list li').length}`);
+
+  // change style with stats
+  changeColor();
+  function changeColor() {
+    ListItem.style.backgroundColor = statsColors[btnStats.innerText.toLowerCase()] || "rgb(201, 231, 130)";
+    btnStats.style.backgroundColor = statsColors[btnStats.innerText.toLowerCase() + " BTN"] || "rgb(201, 231, 130)";
+  }
+
+  //EVENT BUTTON - to change stats
+  btnStats.addEventListener("click", (event) => {
+
     const btn = event.target;
-    if (btn.innerText == "Em Tratativa") {
-      btn.innerText = "Feito";
-      btn.parentNode.style.backgroundColor = "#ccc";
-    } else {
-      btn.innerText = "Em Tratativa";
-      btn.parentNode.style.backgroundColor = "#ebe39e";
-    }
-    let id = btn.parentNode.classList.value.replace("li-", "");
-    tasks[id].splice(1, 0, btn.innerText);
+    let atualOption = options.indexOf(btn.innerText.toLowerCase())
+    atualOption == options.length-1 ? btn.innerText = options[1] : btn.innerText = options[atualOption+1];
+
+    changeColor();
+
+    const li = event.target.parentNode.parentNode;
+    let id = li.classList.value.replace("li-", "");
+    tasks[id].stats = btn.innerText;
     localStorage.setItem('tasks', JSON.stringify(tasks));
   });
-  taskItem.appendChild(description);
-  taskItem.appendChild(spanButton);
-  return taskItem;
+
+
+  //EVENT BUTTON - to remove this li
+  btnRemove.addEventListener("click", (event) => {
+    const itemClass = ListItem.classList.value;
+    const itemToRemove = document.querySelector("." + itemClass);
+    const verify = confirm("Deseja excluir o item: " + itemToRemove.firstChild.innerText);
+
+    if (verify) {
+      tasks.splice(itemClass.replace("li-", ""), 1) // remove this item to the takslist - useful for a DOM.
+      itemToRemove.parentNode.removeChild(itemToRemove) // remove this li only from DOM.
+      localStorage.setItem('tasks', JSON.stringify(tasks)); // update the cache.
+
+    } else {
+      alert("Item não foi removido");
+    }
+
+  })
+
+  ListItem.appendChild(description);
+  divButtons.appendChild(btnStats);
+  divButtons.appendChild(btnRemove);
+  ListItem.appendChild(divButtons);
+
+  return ListItem;
 }
+
