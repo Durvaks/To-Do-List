e4f50d9be44f21750f4996eb0a47157552f18f7c
Renan_Brandao <104158547+Durvaks@users.noreply.github.com>
2023-02-09 16:28:19 -0300

    upgrade 1.1
    In this upgrade, i added new functionality to create other lists and organize with a chose of user.

--- script.js
+++ script.js
@@ -1,96 +1,139 @@
 // Get references to the form and list elements
-const form = document.querySelector("form");
-const taskList = document.querySelector('ul');
+const form = document.querySelector(".form");
+const taskList = document.querySelector('#task-list');
 const btn_clearCache = document.querySelector('.clear_list');
 const btn_clearLastItem = document.querySelector('.clear_last_item');
 const btn_clearFirstItem = document.querySelector('.clear_first_item');
+const btn_newList = document.querySelector('.new_list');
+const btn_lists = document.querySelector('.lists');
+const list_title = document.querySelector('#list_title');
+const clear_all = document.querySelector('.clear_all');
+let menuList = document.querySelector("#other_lists");
+let lastListView = localStorage.getItem("lastlistview") || 0;
+let lists = JSON.parse(localStorage.getItem('lists')) || ["new list"];
+let tasks = JSON.parse(localStorage.getItem(lists[lastListView])) || [];
 
-
-// Load any existing to-dos from cache
-let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
-
-// Render the existing to-dos to the page
-tasks.forEach(task => {
-  taskList.appendChild(createLi(task));
-});
-
-
-// Add a new to-do when the form is submitted
-form.addEventListener('submit', event => {
-  event.preventDefault();
-
-
-
-  // Get the value of the input field
-  const task = event.target.elements.task.value;
-
-  // Get atual time
-  const thisTime = new Date();
-
-  if (task !== "") {
-    const elementLi = createLi(task);
-
-    // Add the task to the tasks array
-    tasks.push({
-      "description": elementLi.firstChild.innerText,
-      "stats": elementLi.lastChild.firstChild.firstChild.classList[1],
-      "creationDate": thisTime
-    });
-
-    // Render the new task to the page
-    taskList.appendChild(elementLi);
-
-    // Save the tasks array to cache
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-
-    // Clear the input field
-    event.target.elements.task.value = '';
+// Event Button - Show list of lists
+btn_lists.addEventListener("click", () => {
+  menuList.style.height == "auto" ? menuList.style.height = '0' : menuList.style.height = 'auto';
+})
+// Event Button - Create a New list
+btn_newList.addEventListener("click", (event) => {
+  let title = prompt("write a name with a new list");
+  if (title !== "" && lists.indexOf(title)) {
+    lists.push(title.toLowerCase());
+    localStorage.setItem('lists', JSON.stringify(lists));
+    realoadMenu();
+    start()
+  }else{
+    alert('invalid or repeat text')
   }
-});
-
-// EVENT BUTTON - clear all
+})
+// Event Button - clear this list // corrigir bug que fica repetindo a pergunta de confirmação
 btn_clearCache.addEventListener("click", () => {
-  const verify = confirm("want to remove all items from the list?");
+  let verify = confirm("want to remove all items from the list?");
   if (verify) {
-    alert("All itens removed")
-    localStorage.clear();
+    localStorage.removeItem(lists[lastListView]);
+    lists.splice(lists.indexOf(lists[lastListView]), 1);
+    if(lists.length == 0){
+      lists.push("new list")
+    }
+    localStorage.setItem('lists', JSON.stringify(lists));
     taskList.innerHTML = "";
+    lastListView = 0;
+    localStorage.setItem("lastlistview", 0);
+    realoadMenu();
+    start()
+    alert("All itens of this list as removed")
   } else {
     alert("removal canceled")
   }
 });
+// Event Button - Clear All
+clear_all.addEventListener("click", ()=>{
+  if(confirm("Clear all lists?")){
+    lists = ["new list"];
+    localStorage.clear();    
+    start();
+    realoadMenu();
+    alert("All lists clear")
+  }
+})
 
-// ######  FUNCTION - create a item List  #######
+start();
+function start() {
+  taskList.innerHTML = "";
+  tasks = JSON.parse(localStorage.getItem(lists[lastListView])) || [];
+// resolver a questão do lastlistview retornar um numero inexistente.
+  list_title.innerText = lists[lastListView] == undefined ? "New List" : lists[lastListView];
+  tasks.forEach(task => {
+    taskList.appendChild(createLi(task));
+  });
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    const task = event.target.elements.task.value;
+    const thisTime = new Date();
+    if (task !== "") {
+      const elementLi = createLi(task);
+      tasks.push({
+        "description": elementLi.firstChild.innerText,
+        "stats": elementLi.lastChild.firstChild.firstChild.classList[1],
+        "creationDate": thisTime
+      });
+      taskList.appendChild(elementLi);
+      localStorage.setItem(lists[lastListView], JSON.stringify(tasks));
+      event.target.elements.task.value = '';
+    }
+  });
+
+}
+realoadMenu()
+function realoadMenu() {
+  menuList.innerHTML = "";
+  lists.forEach((listName) => {
+    if (listName !== null) {
+      const li = document.createElement("li");
+      li.innerText = listName;
+      li.addEventListener("click", (event) => {
+        let choseList = li.innerText.toLowerCase();
+        console.log(choseList);
+        console.log(lists.indexOf(choseList))
+        lastListView = lists.indexOf(choseList);
+        localStorage.setItem("lastlistview", lastListView);
+        taskList.innerHTML = "";
+        menuList.style.height = '0'
+        list_title.innerText = listName;
+        start();
+      })
+      menuList.appendChild(li);
+    }
+  })
+}
 function createLi(task) {
   //create elements and include class
   const ListItem = document.createElement('li');
   const description = document.createElement('span');
   description.classList.add("description");
-
   const btnStats = document.createElement('span');
   btnStats.classList.add("button_li");
   const btnStatsI = document.createElement('i');
   btnStatsI.classList.add("fa-solid");
   btnStats.appendChild(btnStatsI);
-
-
   const btnRemove = document.createElement("span");
   btnRemove.classList.add("btnRemove");
   const btnRemoveI = document.createElement("i")
   btnRemoveI.classList.add("fa-solid", "fa-trash-can");
   btnRemove.appendChild(btnRemoveI);
-
   const divButtons = document.createElement("div");
   divButtons.classList.add("divButtons");
 
-  const options = [
+  const options = [ //options get icons to fontsAwesome
     "fa-plus",
     "fa-hourglass-half",
     "fa-check"
   ];
 
-  const statsColors = {
+  const statsColors = {// define color of buttons of stats
     "fa-plus": "transparent",
     "fa-plus BTN": "white",
 
@@ -101,7 +144,6 @@ function createLi(task) {
     "fa-check BTN": "green"
   }
 
-  // if from cache
   if (typeof (task) == "object") {
     description.innerText = task.description;
     btnStatsI.classList.add(task.stats || 'notClass');
@@ -111,8 +153,6 @@ function createLi(task) {
   }
   ListItem.classList.add(`li-${document.querySelectorAll('#task-list li').length}`);
 
-
-  // change style with stats
   changeColor();
   function changeColor() {
     ListItem.style.backgroundColor = statsColors[btnStats.firstChild.classList[1]] || "rgb(201, 231, 130)";
@@ -121,7 +161,6 @@ function createLi(task) {
 
   //EVENT BUTTON - to change stats
   btnStats.addEventListener("click", (event) => {
-
     const btnIcon = btnStats.firstChild;
     const btnIconClass = btnIcon.classList[1];
     let atualOption = options.indexOf(btnIconClass);
@@ -130,32 +169,25 @@ function createLi(task) {
     const li = btnStats.parentNode.parentNode;
     let id = li.classList.value.replace("li-", "");
     tasks[id].stats = btnIcon.classList[1];
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem(lists[lastListView], JSON.stringify(tasks));
   });
-
 
   //EVENT BUTTON - to remove this li
   btnRemove.addEventListener("click", (event) => {
     const itemClass = ListItem.classList.value;
     const itemToRemove = document.querySelector("." + itemClass);
     const verify = confirm("want to remove this item? " + itemToRemove.firstChild.innerText);
-
     if (verify) {
       tasks.splice(itemClass.replace("li-", ""), 1) // remove this item to the takslist - useful for a DOM.
       itemToRemove.parentNode.removeChild(itemToRemove) // remove this li only from DOM.
-      localStorage.setItem('tasks', JSON.stringify(tasks)); // update the cache.
-
+      localStorage.setItem(lists[lastListView], JSON.stringify(tasks)); // update the cache.
     } else {
       alert("Item has not been removed");
     }
-
   })
-
   ListItem.appendChild(description);
   divButtons.appendChild(btnStats);
   divButtons.appendChild(btnRemove);
   ListItem.appendChild(divButtons);
-
   return ListItem;
 }
-
